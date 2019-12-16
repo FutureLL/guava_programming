c9c3c6c6d789f67c128995a4800994461757b628
futurell <13186102535@163.com>
2019-12-16 15:21:59 +0800

    Create MonitorExample class!

---
+++ src/main/java/com/lilei/guava/concurrent/MonitorExample.java
@@ -0,0 +1,11 @@
+package com.lilei.guava.concurrent;
+
+/**
+ * @description:
+ * @author: Mr.Li
+ * @date: Created in 2019/12/16 15:21
+ * @version: 1.0
+ * @modified By:
+ */
+public class MonitorExample {
+}
